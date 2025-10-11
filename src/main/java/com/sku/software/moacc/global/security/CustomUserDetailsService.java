fcb79c856ff21553f5a7b050505c1dccdddc67ee
jaeyeon <kasto6329@gmail.com>
2025-10-11 11:28:04 +0900

    lombok 어노테이션 사용

--- src/main/java/com/sku/software/moacc/global/security/CustomUserDetailsService.java
+++ src/main/java/com/sku/software/moacc/global/security/CustomUserDetailsService.java
@@ -16,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByUsername(username)
+    User user = userRepository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
     return new CustomUserDetails(user);
   }
