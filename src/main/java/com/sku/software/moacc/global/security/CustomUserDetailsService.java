0e7eb2f34f896b6f6aa2d666a9af3ab941525a39
jaeyeon <kasto6329@gmail.com>
2025-10-11 12:01:17 +0900

    readonly tranaction 추가

--- src/main/java/com/sku/software/moacc/global/security/CustomUserDetailsService.java
+++ src/main/java/com/sku/software/moacc/global/security/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
+  @Transactional(readOnly = true)
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository.findByEmail(username)
